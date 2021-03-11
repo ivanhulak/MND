86f8ddc9f45af6220176a82f7a4b6699d460da10
ivanhulak <g.ivan.gulak17@gmail.com>
2021-03-11 12:46:09 +0200

    Made a personal task

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -90,62 +90,66 @@ ruv = []
 ruv.append(abs(tetta[0] - 1) / sigma_tetta)
 ruv.append(abs(tetta[1] - 1) / sigma_tetta)
 ruv.append(abs(tetta[2] - 1) / sigma_tetta)
+print(f"ruv: {ruv}")
 
 # 7) Перевіримо на однорідність дисперсію
 r_kr = 2
 for i in range(len(ruv)):
     if ruv[i] > r_kr:
         print("Неоднорідна дисперсія")
+    # Додаткове завдання: Ви не можете переходити до "етапу 8" (на 101) якщо дисперсія неоднорідна
+    else:
+        # 8) Розрахунок нормованих коефіцієнтів рівняння регресії
+        mx1 = (xn[0][0] + xn[1][0] + xn[2][0]) / 3
+        mx2 = (xn[0][1] + xn[1][1] + xn[2][1]) / 3
+        my = sum(avg_y) / 3
+
+        a1 = (xn[0][0] ** 2 + xn[1][0] ** 2 + xn[2][0] ** 2) / 3
+        a2 = (xn[0][0] * xn[0][1] + xn[1][0] * xn[1][1] + xn[2][0] * xn[2][1]) / 3
+        a3 = (xn[0][1] ** 2 + xn[1][1] ** 2 + xn[2][1] ** 2) / 3
+
+        a11 = (xn[0][0] * avg_y[0] + xn[1][0] * avg_y[1] + xn[2][0] * avg_y[2]) / 3
+        a22 = (xn[0][1] * avg_y[0] + xn[1][1] * avg_y[1] + xn[2][1] * avg_y[2]) / 3
+
+        b0 = determinant(my, mx1, mx2, a11, a1, a2, a22, a2, a3) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
+        b1 = determinant(1, my, mx2, mx1, a11, a2, mx2, a22, a3) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
+        b2 = determinant(1, mx1, my, mx1, a1, a11, mx2, a2, a22) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
+
+        print(f"b0: {b0}")
+        print(f"b1: {b1}")
+        print(f"b2: {b2}")
+
+        # Отже, нормовані рівняння регресії
+        y_pr1 = b0 + b1 * xn[0][0] + b2 * xn[0][1]
+        y_pr2 = b0 + b1 * xn[1][0] + b2 * xn[1][1]
+        y_pr3 = b0 + b1 * xn[2][0] + b2 * xn[2][1]
+
+        # Проведемо натуралізацію коефіцієнтів:
+        dx1 = abs(x1_max - x1_min) / 2
+        dx2 = abs(x2_max - x2_min) / 2
+        x10 = (x1_max + x1_min) / 2
+        x20 = (x2_max + x2_min) / 2
+
+        a_0 = b0 - (b1 * x10 / dx1) - (b2 * x20 / dx2)
+        a_1 = b1 / dx1
+        a_2 = b2 / dx2
+
+        # Запишемо натуралізоване рівняння регресії:
+        yP1 = a_0 + a_1 * x1_min + a_2 * x2_min
+        yP2 = a_0 + a_1 * x1_max + a_2 * x2_min
+        yP3 = a_0 + a_1 * x1_min + a_2 * x2_max
+
+        print('Експериментальні значення критерію Романовського:')
+        for i in range(3):
+            print(ruv[i])
+
+        print('Натуралізовані коефіцієнти: \na0 =', round(a_0, 4), 'a1 =', round(a_1, 4), 'a2 =', round(a_2, 4))
+        print('У практичний: ', round(y_pr1, 4), round(y_pr2, 4), round(y_pr3, 4))
+        print('У середній:', round(avg_y[0], 4), round(avg_y[1], 4), round(avg_y[2], 4))
+        print('У практичний норм.', round(yP1, 4), round(yP2, 4), round(yP3, 4))
 
 
-# 8) Розрахунок нормованих коефіцієнтів рівняння регресії
-mx1 = (xn[0][0] + xn[1][0] + xn[2][0]) / 3
-mx2 = (xn[0][1] + xn[1][1] + xn[2][1]) / 3
-my = sum(avg_y) / 3
-
-a1 = (xn[0][0] ** 2 + xn[1][0] ** 2 + xn[2][0] ** 2) / 3
-a2 = (xn[0][0] * xn[0][1] + xn[1][0] * xn[1][1] + xn[2][0] * xn[2][1]) / 3
-a3 = (xn[0][1] ** 2 + xn[1][1] ** 2 + xn[2][1] ** 2) / 3
-
-a11 = (xn[0][0] * avg_y[0] + xn[1][0] * avg_y[1] + xn[2][0] * avg_y[2]) / 3
-a22 = (xn[0][1] * avg_y[0] + xn[1][1] * avg_y[1] + xn[2][1] * avg_y[2]) / 3
-
-b0 = determinant(my, mx1, mx2, a11, a1, a2, a22, a2, a3) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
-b1 = determinant(1, my, mx2, mx1, a11, a2, mx2, a22, a3) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
-b2 = determinant(1, mx1, my, mx1, a1, a11, mx2, a2, a22) / determinant(1, mx1, mx2, mx1, a1, a2, mx2, a2, a3)
-
-print(f"b0: {b0}")
-print(f"b1: {b1}")
-print(f"b2: {b2}")
 
 
-# Отже, нормовані рівняння регресії
-y_pr1 = b0 + b1 * xn[0][0] + b2 * xn[0][1]
-y_pr2 = b0 + b1 * xn[1][0] + b2 * xn[1][1]
-y_pr3 = b0 + b1 * xn[2][0] + b2 * xn[2][1]
 
 
-# Проведемо натуралізацію коефіцієнтів:
-dx1 = abs(x1_max - x1_min) / 2
-dx2 = abs(x2_max - x2_min) / 2
-x10 = (x1_max + x1_min) / 2
-x20 = (x2_max + x2_min) / 2
-
-a_0 = b0 - (b1 * x10 / dx1) - (b2 * x20 / dx2)
-a_1 = b1 / dx1
-a_2 = b2 / dx2
-
-# Запишемо натуралізоване рівняння регресії:
-yP1 = a_0 + a_1 * x1_min + a_2 * x2_min
-yP2 = a_0 + a_1 * x1_max + a_2 * x2_min
-yP3 = a_0 + a_1 * x1_min + a_2 * x2_max
-
-
-print('Експериментальні значення критерію Романовського:')
-for i in range(3):
-    print(ruv[i])
-
-print('Натуралізовані коефіцієнти: \na0 =', round(a_0, 4), 'a1 =', round(a_1, 4), 'a2 =', round(a_2, 4))
-print('У практичний: ', round(y_pr1, 4), round(y_pr2, 4), round(y_pr3, 4))
-print('У середній:', round(avg_y[0], 4), round(avg_y[1], 4), round(avg_y[2], 4))
-print('У практичний норм.', round(yP1, 4), round(yP2, 4), round(yP3, 4))
